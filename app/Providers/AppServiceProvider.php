a914176af8819017114e8764ae55c4727f4eb5d2
ERIC VARGAS <eric.vargas.rss@gmail.com>
2018-09-06 14:01:59 -0500

    Login UI advances

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;//Libreria necesaria para el uso de migrate
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //linea para la instalacion correcta de los migrate
+        Schema::defaultStringLength(191);
     }
 
     /**
